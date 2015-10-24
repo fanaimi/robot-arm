b28f1868b2782bc25d179648e1c3a5769afac87e
fanaimi <emiliano.fantasia@gmail.com>
2015-10-24 23:38:42 +0100

    easy keyboard control - arrow keys, R/F, T/G

--- studios/1-keys_ctrl.js
+++ studios/1-keys_ctrl.js
@@ -12,8 +12,19 @@ var keypress = require('keypress');
 var _port = "/dev/tty.usbmodem1411"
     ,   _board = new j5.Board(_port)
     ,   _pin = 9
+    ,   _pin1 = 10
+    ,   _pin2 = 11
+    ,   _pin3 = 12
+
     ,   _servo
+    ,   _servo1
+    ,   _servo2
+    ,   _servo3
+
     ,   a = 90
+    ,   a1 = 90
+    ,   a2 = 90
+    ,   a3 = 125
     ;
 
 
@@ -23,15 +34,24 @@ keypress(process.stdin);
 _board.on("ready", function(){
     console.log("ready");
     _servo = new j5.Servo(_pin);
+    _servo1 = new j5.Servo(_pin1);
+    _servo2 = new j5.Servo(_pin2);
+    _servo3 = new j5.Servo(_pin3);
 
 
 
     this.repl.inject({
-        board   : _board,
-        s       : _servo
+        board   : _board
+        , s       : _servo
+        , s1       : _servo1
+        , s2       : _servo2
+        , s3       : _servo3
     });
 
     _servo.to(a, 500);
+    _servo1.to(a1, 500);
+    _servo2.to(a2, 500);
+    _servo3.to(a3, 500);
 
 
 
@@ -44,43 +64,100 @@ _board.on("ready", function(){
 
         if ( !key ) return;
 
+        console.log("=> " +key.name);
 
         if ( key.name == 'q' ) {
-
-            console.log('Quitting');
+            console.log('Quitting and resetting');
+            _servo.to(90, 500);
+            _servo1.to(90, 500);
+            _servo2.to(90, 500);
+            _servo3.to(125, 500);
             process.exit();
+        }//quit
 
-        } else if ( key.name == 'up' ) {
 
+        /**
+         *  UP and DOWN for servo1
+         *
+         */
+        else if ( key.name == 'up' ) {
             console.log('up');
-
-        } else if ( key.name == 'down' ) {
-
-            console.log('down');
-
-        } else if ( key.name == 'left' ) {
-
-            console.log('Left');
-
-
-            if(a<180){
-                _servo.to(a+=10);
+            if(a1 < 180){
+                _servo1.to(a1+=5);
             }
+            // ================= / up
+        } else if ( key.name == 'down' ) {
+            console.log('down');
+            if(a1 >0){
+                _servo1.to(a1-=5);
+            }
+        }// ================= / down
 
 
+
+
+        /**
+         *  LEFT and RIGHT for servo
+         *  BASE ROTATION
+         */
+        else if ( key.name == 'left' ) {
+            console.log('Left');
+            if(a<180){
+                _servo.to(a+=5);
+            }
             //========== / left
         } else if ( key.name == 'right' ) {
-
             console.log('Right');
-
-
             if(a>0){
-                _servo.to(a-=10);
+                _servo.to(a-=5);
             }
+        }//========== / right
 
-            //========== / right
 
-        } else if ( key.name == 'space' ) {
+        /**
+         *  R and F for servo2
+         *  UP / DOWN
+         */
+        else if ( key.name == 'r' ) {
+            console.log('r');
+            if(a2>0){
+                _servo2.to(a2-=5);
+            }
+            //========== / R
+
+        } else if ( key.name == 'f' ) {
+            console.log('f');
+            if(a2<180){
+                _servo2.to(a2+=5);
+            }
+        }//========== / F
+
+
+
+
+
+        /**
+         *  T and G for servo3 -CLAW
+         *  OPEN / CLOSE
+         */
+        else if ( key.name == 't' ) {
+            console.log('t ' + a3);
+            if(a3>125){
+                _servo3.to(a3-=5);
+            }
+            //========== / R
+
+        } else if ( key.name == 'g' ) {
+            console.log('g ' + a3);
+            if(a3<165){
+                _servo3.to(a3+=5);
+            }
+        }//========== / F
+
+
+
+
+        else if ( key.name == 'space=====' ) {
 
             console.log('Stopping');
 
